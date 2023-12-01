0c09362dc4d0a79e44c41a6acd9118631e81abbe
samusesapple <gwansammy@gmail.com>
2023-12-01 14:03:01 +0900

    State, setState({ state값 변경 내용 })

--- rn_01/App.js
+++ rn_01/App.js
@@ -5,19 +5,54 @@
  * @format
  */
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import React, { Component } from 'react';
+
 import { View, Text, StyleSheet } from 'react-native';
 
 
-function App(): JSX.Element {
-  return (
-    <View style={styles.background}>
-      <Text> Hello World </Text>
-    </View>
-  );
+class App extends Component {
+
+  // state는 class 내부에서만 사용 가능, render() 전에 선언
+  // state의 값에 따라 화면을 다르게 구성할 수 있음
+  state = {
+    sampleText: 'Hello World',
+    sampleBoolean: true
+  }
+
+  // state.sampleBoolean 값이 토글되도록 state 변경하기
+  changeState = () => {
+    if (!this.state.sampleBoolean) {
+      this.setState({
+        sampleBoolean: true,
+        sampleText: 'sampleBoolean is True'
+      })
+    } else {
+      this.setState({
+        sampleBoolean: false,
+        sampleText: 'sampleBoolean is False!'
+      })
+    }
+  }
+
+  inputText = () => (
+    this.state.sampleBoolean ? 
+    <Text>sampleBoolean is True</Text>
+    :
+    <Text>sampleBoolean is False</Text>
+  )
+
+  render() {
+    return (
+      <View style={styles.background}>
+        <Text onPress={this.changeState}>
+          {this.inputText()}
+        </Text>
+      </View>
+    );
+  }
 }
 
+// view의 style을 주기 위한 상수
 const styles = StyleSheet.create({
   background: {
     flex: 1,
